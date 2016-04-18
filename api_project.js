1269902fc990e587541c632e39e1a642b6474d48
CI Build <rrdelaney@outlook.com>
2016-04-18 21:12:41 +0000

    CI Build

---
+++ api_project.js
@@ -0,0 +1,14 @@
+define({
+  "title": "SFAPI docs",
+  "sampleUrl": "https://reoz4sq35d.execute-api.us-east-1.amazonaws.com/prod",
+  "name": "SuperFeed",
+  "version": "0.0.0",
+  "description": "",
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2016-04-18T21:12:41.447Z",
+    "url": "http://apidocjs.com",
+    "version": "0.15.1"
+  }
+});
